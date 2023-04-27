a0bec51b0ec1aa33f18f2cf55073183007af8243
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-27 18:57:52 +0530

    introducing table

---
+++ src/EmpListing.js
@@ -0,0 +1,27 @@
+const EmpListing = () => {
+    return (<div className="container">
+        <div className="card">
+            <div className="card-title">
+                <h2> Employee Listing </h2>
+            </div>
+            <div className="card-body">
+                <table className="table table-bordered">
+                    <thead className="bg-dark text-white">
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Phone</td>
+                        <td>Action</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>);
+}
+
+export default EmpListing;
