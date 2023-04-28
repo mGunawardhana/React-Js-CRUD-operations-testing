fd417b4f717554282ae4150e477376c6d92aeb8a
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-28 09:47:16 +0530

    introducing functions

--- src/EmpCreate.js
+++ src/EmpCreate.js
@@ -1,104 +1,130 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+
 const EmpCreate = () => {
-  const [id, idChange] = useState("");
-  const [name, nameChange] = useState("");
-  const [email, emailChange] = useState("");
-  const [phone, phoneChange] = useState("");
-  const [active, activeChange] = useState(true);
+    const [id, idChange] = useState("");
+    const [name, nameChange] = useState("");
+    const [email, emailChange] = useState("");
+    const [phone, phoneChange] = useState("");
+    const [active, activeChange] = useState(true);
+    const [validation, valChange] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(id, name, email, phone, active);
-  };
 
-  return (
-    <div>
-      <div className="row">
-        <div className="offset-lg-3 col-lg-6">
-          <form className="container" onSubmit={handleSubmit}>
-            <div className="card" style={{ textAlign: "left" }}>
-              <div className="card-title">
-                <h2>Employee Create</h2>
-              </div>
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <label>ID</label>
-                      <input
-                        value={id}
-                        disabled
-                        className="form-control"
-                        onChange={(e) => idChange(e.target.value)}
-                      ></input>
-                    </div>
-                  </div>
 
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <label>Name</label>
-                      <input
-                        value={name}
-                        onChange={(e) => nameChange(e.target.value)}
-                        className="form-control"
-                      ></input>
-                    </div>
-                  </div>
+    const navigate = useNavigate();
 
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <label>Email</label>
-                      <input
-                        value={email}
-                        onChange={(e) => emailChange(e.target.value)}
-                        className="form-control"
-                      ></input>
-                    </div>
-                  </div>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const empData
+            = {name, email, phone, active};
 
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <label>Phone</label>
-                      <input
-                        value={phone}
-                        onChange={(e) => phoneChange(e.target.value)}
-                        className="form-control"
-                      ></input>
-                    </div>
-                  </div>
+        fetch(" http://localhost:8000/employee", {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(empData)
+        }).then((resp) => {
+            alert("Saved Successfully!");
+            navigate('/');
+        }).catch((error) => {
+            alert(error.getMessage);
+        })
 
-                  <div className="col-lg-12">
-                    <div className="form-check">
-                      <input
-                        checked={active}
-                        type="checkbox"
-                        className="form-check"
-                        onChange={(e) => activeChange(e.target.checked)}
-                      ></input>
-                      <label className="form-check-label">Is Active </label>
-                    </div>
-                  </div>
 
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <button type="submit" className="btn btn-success">
-                        Save
-                      </button>
-                      <Link to={"/"} className="btn btn-danger">
-                        Back
-                      </Link>
-                    </div>
-                  </div>
+    };
+
+    return (
+        <div>
+            <div className="row">
+                <div className="offset-lg-3 col-lg-6">
+                    <form className="container" onSubmit={handleSubmit}>
+                        <div className="card" style={{textAlign: "left"}}>
+                            <div className="card-title">
+                                <h2>Employee Create</h2>
+                            </div>
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="form-group">
+                                            <label>ID</label>
+                                            <input
+                                                value={id}
+                                                disabled
+                                                className="form-control"
+                                                onChange={(e) => idChange(e.target.value)}
+                                            ></input>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                        <div className="form-group">
+                                            <label>Name</label>
+                                            <input
+                                                required
+                                                value={name}
+                                                onMouseDown={e=>valChange(true)}
+                                                onChange={(e) => nameChange(e.target.value)}
+                                                className="form-control"
+                                            ></input>
+                                            {name.length == 0 && validation && <span className="text-danger">Enter the name</span>
+                                               }
+
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                        <div className="form-group">
+                                            <label>Email</label>
+                                            <input
+                                                value={email}
+                                                onChange={(e) => emailChange(e.target.value)}
+                                                className="form-control"
+                                            ></input>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                        <div className="form-group">
+                                            <label>Phone</label>
+                                            <input
+                                                value={phone}
+                                                onChange={(e) => phoneChange(e.target.value)}
+                                                className="form-control"
+                                            ></input>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                        <div className="form-check">
+                                            <input
+                                                checked={active}
+                                                type="checkbox"
+                                                className="form-check"
+                                                onChange={(e) => activeChange(e.target.checked)}
+                                            ></input>
+                                            <label className="form-check-label">Is Active </label>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                        <div className="form-group">
+                                            <button type="submit" className="btn btn-success">
+                                                Save
+                                            </button>
+                                            <Link to={"/"} className="btn btn-danger">
+                                                Back
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-              </div>
             </div>
-          </form>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
+//28:08
 export default EmpCreate;
--- src/EmpListing.js
+++ src/EmpListing.js
@@ -1,64 +1,84 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
 //tofu:
 const EmpListing = () => {
-  const [empData, empDataChange] = useState(null);
-  useEffect(() => {
-    fetch("http://localhost:8000/employee")
-      .then((res) => {
-        return res.json();
-      })
-      .then((resp) => {
-        // alert(resp.getMessage())
-        empDataChange(resp);
-      })
-      .catch((error) => {
-        alert(error.getMessage());
-      });
-  }, []);
+    const [empData, empDataChange] = useState(null);
+    const navigate = useNavigate();
+    const LoadDetail = (id) => {
 
-  return (
-    <div className="container">
-      <div className="card">
-        <div className="card-title">
-          <h2> Employee Listing </h2>
-        </div>
-        <div className="card-body">
-          <div className="pb-4 divBtn">
-            <Link to="employee/create" className="btn btn-primary">Add New (+) </Link>
-          </div>
+    }
 
-          <table className="table table-bordered">
-            <thead className="bg-dark text-white">
-              <tr>
-                <td>ID</td>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Phone</td>
-                <td>Action</td>
-              </tr>
-            </thead>
-            <tbody>
-              {empData &&
-                empData.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.name}</td>
-                    <td>{item.email}</td>
-                    <td>{item.phone}</td>
-                    <td className="">
-                      <a className="btn btn-success">Edit</a>
-                      <a className="btn btn-danger">Remove</a>
-                      <a className="btn btn-primary">Details</a>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+    const LoadEdit = (id) => {
+
+    }
+
+    const RemoveFunction = (id) => {
+
+    }
+
+
+    useEffect(() => {
+        fetch("http://localhost:8000/employee")
+            .then((res) => {
+                return res.json();
+            })
+            .then((resp) => {
+                // alert(resp.getMessage())
+                empDataChange(resp);
+            })
+            .catch((error) => {
+                alert(error.getMessage());
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card-title">
+                    <h2> Employee Listing </h2>
+                </div>
+                <div className="card-body">
+                    <div className="pb-4 divBtn">
+                        <Link to="employee/create" className="btn btn-primary">Add New (+) </Link>
+                    </div>
+
+                    <table className="table table-bordered">
+                        <thead className="bg-dark text-white">
+                        <tr>
+                            <td>ID</td>
+                            <td>Name</td>
+                            <td>Email</td>
+                            <td>Phone</td>
+                            <td>Action</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {empData &&
+                            empData.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.phone}</td>
+                                    <td className="">
+                                        <a onClick={() => {
+                                            LoadEdit(item.id)
+                                        }} className="btn btn-success">Edit</a>
+                                        <a onClick={() => {
+                                            RemoveFunction(item.id)
+                                        }} className="btn btn-danger">Remove</a>
+                                        <a onClick={() => {
+                                            LoadDetail(item.id)
+                                        }} className="btn btn-primary">Details</a>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EmpListing;
